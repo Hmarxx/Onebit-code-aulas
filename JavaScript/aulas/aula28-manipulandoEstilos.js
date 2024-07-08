a767675fd096f80b094d0f8d239994359a7b32a0
Hmarxx <henrymarx@hotmail.com>
2024-07-08 15:20:29 -0300

    Aula - correção

--- JavaScript/aulas/aula28-manipulandoEstilos.js
+++ JavaScript/aulas/aula28-manipulandoEstilos.js
@@ -1,11 +1,11 @@
 function useLightTheme (){
-    document.body.style.backgroundColor = '#f1f5f9' 
     document.body.style.color = '#212529'
+    document.body.style.backgroundColor = '#f1f5f9' 
 }
 
 function useDarkTheme(){
-    document.body.style.backgroundColor = '#212529'
     document.body.style.color = '#f1f5f9'
+    document.body.style.backgroundColor = '#212529'
 }
 
 function switchTheme(){
